5b4e2587dc19849318cdcde4b66882ebc8fcf7e3
Fernando Nogueira <mrfernando2509@gmail.com>
2021-06-20 07:32:19 -0400

    Fix Procfile again

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 from app import create_app, plugins
+import sys
 
 v = create_app()
-plugins.sio.run(v)
+
+if len(sys.argv) == 3:
+    plugins.sio.run(v, host=sys.argv[1], port=sys.argv[2])
+else:
+    print("You haven't specified a host/port")
+    plugins.sio.run(v)
